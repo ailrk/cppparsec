d54c66081c3d3cbb9519588c0d9e04b9c04569ae
Jimmy <jimmy123good@hotmail.com>
2020-11-22 06:27:02 -0800

    new

---
+++ include/parser.h
@@ -0,0 +1,117 @@
+#ifndef CPPPARSEC_PARSER_
+#define CPPPARSEC_PARSER_
+
+#include "stream.h"
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <variant>
+
+namespace cppparsec {
+
+// Monadic parser combinator
+template <typename S, typename T> class Parser {
+public:
+  using InputStreamType = S;
+  struct Ok {
+    std::unique_ptr<InputStreamType> stream;
+    T val;
+  };
+
+  struct Error {
+    std::unique_ptr<InputStreamType> stream;
+    const std::string error_message;
+  };
+
+  using Result = std::variant<Ok, Error>;
+  using RunParserFnType =
+      std::function<Result(std::unique_ptr<InputStreamType>)>;
+
+  RunParserFnType run_parser;
+
+  Parser() = delete;
+
+  // we don't want to copy parser, because that will copy the internal
+  // state as well.
+  Parser(const Parser &) = delete;
+  Parser &operator=(const Parser &) = delete;
+
+  Parser(RunParserFnType &&f) : run_parser(f){};
+
+  template <typename U> auto map(std::function<U(T)> &&f) -> Parser<S, U>;
+
+  template <typename U> static auto pure(U) -> Parser<S, U>;
+
+  template <typename U>
+  auto ap(Parser<S, std::function<U(T)>> &fa) -> Parser<S, U>;
+
+  template <typename U>
+  auto then(std::function<Parser<S, U>(T)> &f) -> Parser<S, U>;
+
+  template <typename U>
+  auto operator>>=(std::function<Parser<S, U>(T)> &f) -> Parser<S, U> {
+    return then(std::forward(f));
+  }
+};
+
+template <typename S, typename T>
+template <typename U>
+auto Parser<S, T>::map(std::function<U(T)> &&f) -> Parser<S, U> {
+  return Parser<S, U>([=](auto stream) {
+    // all stream will be moved. The next state can be get from
+    // the returned Result type.
+    auto result = run_parser(std::move(stream));
+    auto [stream1, val1] = std::move(std::get<Ok>(result));
+    return typename Parser<S, U>::Ok{std::move(stream1), f(val1)};
+  });
+}
+
+template <typename S, typename T>
+template <typename U>
+auto Parser<S, T>::pure(U v) -> Parser<S, U> {
+  return Parser<S, U>([=](auto stream) {
+    return typename Parser<S, U>::Ok{std::move(stream), v};
+  });
+}
+
+template <typename S, typename T>
+template <typename U>
+auto Parser<S, T>::ap(Parser<S, std::function<U(T)>> &fa) -> Parser<S, U> {
+  return Parser<S, U>([=](auto stream) {
+    // run fa to get the function.
+    auto fa_result = fa.run_parser(std::move(stream));
+    auto [stream1, f] = std::move(std::get<decltype(fa)::Ok>(fa_result));
+
+    // apply function to the result of run_parser.
+    auto [stream2, result] = run_parser(std::move(stream1));
+    return typename Parser<S, U>::Ok(std::move(stream2), f(result));
+  });
+}
+
+// Monad bind
+template <typename S, typename T>
+template <typename U>
+auto Parser<S, T>::then(std::function<Parser<S, U>(T)> &fma) -> Parser<S, U> {
+  return Parser<S, U>([=](auto stream) {
+    // run self
+    auto result = run_parser(std::move(stream));
+    auto [stream1, v1] = std::move(std::get<Ok>(result));
+
+    // apply f to get a new parser.
+    Parser<S, U> ma = fma(v1);
+    auto ma_result = ma.run_parser(std::move(stream1));
+    auto [stream2, v2] = std::move(std::get<decltype(ma)::Ok>(ma_result));
+
+    return typename Parser<S, U>::Ok(std::move(stream2), v2);
+  });
+}
+
+template <typename T> using SP = Parser<stream::StringStream, T>;
+
+// template <typename S, T>
+// template <typename U>
+// auto Parser<S, T>::in
+
+} // namespace cppparsec
+#endif /* ifndef CPPPARSEC_COMBINATOR_ */
---
+++ include/stream.h
@@ -0,0 +1,67 @@
+#ifndef CPPPARSEC_STREAM_
+#define CPPPARSEC_STREAM_
+
+#include <cassert>
+#include <memory>
+#include <string_view>
+
+namespace cppparsec {
+
+struct Position {
+  size_t line;
+  size_t col;
+};
+
+namespace stream {
+template <typename T> class Stream {
+public:
+  using StreamItemType = T;
+  virtual ~Stream() = default;
+  Stream() = default;
+  Stream(const Stream &) = delete;
+  Stream &operator=(const Stream &) = delete;
+
+  virtual size_t get_line() const = 0;
+  virtual size_t get_col() const = 0;
+  virtual const T *peek_stream() const = 0;
+  virtual std::unique_ptr<Stream<T>> eat(size_t n) const = 0;
+};
+class StringStream : public Stream<std::string_view> {
+
+private:
+  std::string_view data;
+  mutable Position position;
+
+public:
+  StringStream(std::string_view s) : data(s), position(Position{1, 1}) {}
+  StringStream(const std::string_view &s, const Position &pos)
+      : data(s), position(pos) {}
+
+  size_t get_line() const override { return position.line; };
+  size_t get_col() const override { return position.col; };
+  virtual const std::string_view *peek_stream() const override {
+    return &data;
+  };
+
+  std::unique_ptr<Stream<StreamItemType>> eat(size_t n) const override {
+    return std::make_unique<StringStream>(data.substr(n), [=]() {
+      assert(data.size() >= n);
+      Position new_position = position;
+      for (int i = 0; i < n; ++i) {
+        if (data[i] == '\n') {
+          new_position.line++;
+          new_position.col = 1;
+        } else {
+          new_position.col++;
+        }
+      }
+      return new_position;
+    }());
+  };
+};
+
+} // namespace stream
+
+} // namespace cppparsec
+
+#endif /* ifndef CPPPARSEC_COMBINATOR_ */
